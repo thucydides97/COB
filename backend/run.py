93c783f0c102d2a52b0de03a86d5c92c6a66a8ff
thucydides97 <jakebayley97@gmail.com>
2019-12-09 14:23:28 +0000

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,6 @@
 from app import app, db
 from app.models import *
 
-# @app.shell_context_processor
-# def make_shell_context():
-#     return {'db': db, 'User': User, 'Investment': Investment, 'ETF': ETF, 'Stock': Stock, 'Invested': Invested}
+
+if __name__ == "__main__":
+    app.run()
